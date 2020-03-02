8a1e5d2372e6115ff1a7805c87a7432eed0a0377
LuisAlvarez98 <luisfeal1998@hotmail.com>
2020-03-02 15:21:40 +0100

    Default and Named Arguments

---
+++ src/Lab03/Ex1.scala
@@ -0,0 +1,25 @@
+package Lab03
+import definitions.de._
+
+object Ex1 extends App {
+  def parallel[A, B](a: => A, b: => B): (A, B) = {
+    var resultA: A = 1.asInstanceOf[A]
+    var resultB: B = 1.asInstanceOf[B]
+    println(a)
+    println(resultB)
+    val threadA = thread {
+      a
+    }
+    println(threadA)
+    val threadB = thread {
+      b
+    }
+    println(threadB)
+    (resultA, resultB)
+  }
+  val task1 = {3+4}
+  val task2 = {3*1}
+  println (parallel (task1, task2))
+  log ("FIN")
+
+}
--- src/lectures/part1basics/DefaultArgs.scala
+++ src/lectures/part1basics/DefaultArgs.scala
@@ -1,5 +1,10 @@
 package lectures.part1basics
 
 object DefaultArgs extends App {
+  def func(n: Int, acc: Int = 1): Int = {
+    acc
+  }
 
+  val t = func(n = 10, acc = 10)
+  println(t)
 }
